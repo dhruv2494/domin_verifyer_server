057a44142b394b06d528d54ac346699c0bc50661
dhruv.bhuva <dhruv.bhuva@crestinfotech.co.in>
2024-11-14 15:40:00 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const dns = require("dns");
+const cors = require("cors"); // Add this line
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(cors()); // Enable CORS for all routes
+app.use(express.json());
+
+app.get("/verify-domain", (req, res) => {
+  const domain = req.query.domain;
+
+  if (!domain) {
+    return res
+      .status(400)
+      .json({ error: "Domain query parameter is required" });
+  }
+
+  dns.resolveMx(domain, (err, addresses) => {
+    if (err || addresses.length === 0) {
+      return res
+        .status(400)
+        .json({ isValid: false, error: "Domain has no valid MX records" });
+    }
+    return res.json({ isValid: true });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
